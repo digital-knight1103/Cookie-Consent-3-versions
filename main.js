40422c30ab5bbb0c2e9770084ae67b5c8f030a8f
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-04-14 16:20:51 +0200

    add buuton delete cookies

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const cookieBox = document.querySelector('.cookie__wrapper');
-const btn = document.querySelectorAll('.cookie__btn')
-
+const btn = document.querySelectorAll('.cookie__btn');
+const deleteCookies = document.querySelector('#deleteCookies');
 
 const cookieMsg = () => {
   // if cookie contains digitalKnight it will be returned and below of this code will not run
@@ -15,12 +15,20 @@ const cookieMsg = () => {
       if(button.id == 'acceptBtn') {
         console.log('clickAcceptBtn');
         // set cookies for 1 mouth
-        document.cookie = 'cookieBy = digitalKnight, max-age' + 60 * 60 * 24 * 30
+        document.cookie = 'cookieBy = digitalKnight, max-age ' + 60 * 60 * 24 * 30 + 'SameSite=None; Secure'
       }
     })
   })
 }
 
+// Additional Funcionality to tested delete cookies
+deleteCookies.addEventListener('click', () => {
+  document.cookie.split(";").forEach((cookie) => {
+    const [name, value] = cookie.split("=").map((c) => c.trim());
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  });
+  cookieBox.classList.remove('show');
+});
 
 
 window.addEventListener('load', cookieMsg)
